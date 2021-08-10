f7eb733a9103bbb1189ebdf36c04d6c5c3466c64
Sezgin Öztürk <szgnoztrk@gmail.com>
2021-08-10 17:14:35 +0300

    Main.java updated.

--- app/src/dev/patika/clients/Main.java
+++ app/src/dev/patika/clients/Main.java
@@ -36,8 +36,7 @@ public class Main {
             }
             System.out.println("");
         }
-        
-        System.out.println("Try...");
+
     }
 
     private static int checkTestData() {
